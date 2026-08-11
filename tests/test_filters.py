78f492f3c7e2bbdc815f7d724470ed2e586b916e
bcbowen <bcbowen@gmail.com>
2026-08-10 21:01:35 -0400

    working on filters

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,4 +1,4 @@
-from filters import DesignationFilter, NameFilter
+from filters import DesignationFilter, NameFilter, DiameterFilter
 from models import NearEarthObject, CloseApproach
 from datetime import datetime
 import math
@@ -51,3 +51,38 @@ class FilterTests(unittest.TestCase):
             a = CloseApproach("2026-Aug-02 14:34", 1.2, 1.2, n)
             result = f(a)  
             self.assertEqual(expected, result)
+
+    def test_diameter_filter_match(self): 
+        designation = 'n1'
+        name = 'HugeRock'
+        # cases: neo diameter, op, criteria diameter: all expected to match
+        cases = [
+            (2.0, operator.eq, 2.0), 
+            (2.0, operator.ge, 2.0), 
+            (2.0, operator.le, 2.0),
+            (2.0, operator.lt, 2.1), 
+            (2.0, operator.ge, 1.9), 
+            (2.0, operator.lt, 1.9),
+        ]
+
+        n = NearEarthObject(designation, True, name=name)
+        a = CloseApproach("2026-Aug-02 14:34", 1.2, 1.2, n)
+        
+        f = NameFilter(operator.eq, name)
+        expected = True
+        result = f(a) 
+        self.assertEqual(expected, result)
+    
+    def test_diameter_filter_mismatch(self): 
+        designation = 'n1'
+        name = 'HugeRock'
+        bad_names = ['TinyRock', '', None]
+        
+        f = NameFilter(operator.eq, name)
+
+        expected = False
+        for bad_name in bad_names: 
+            n = NearEarthObject(designation, True, name=bad_name)
+            a = CloseApproach("2026-Aug-02 14:34", 1.2, 1.2, n)
+            result = f(a)  
+            self.assertEqual(expected, result)            
